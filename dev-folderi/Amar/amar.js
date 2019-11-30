0bbf16776d40c55011961d91a1dbc0a88c2f1295
amarmujk99 <amarmujkanovic99@gmail.com>
2019-11-30 18:48:46 +0100

    modified:   dev-folderi/Amar/amar.js

--- dev-folderi/Amar/amar.js
+++ dev-folderi/Amar/amar.js
@@ -1,1 +1,33 @@
-console.log('Gojko');
+var kombinacija = [7,8,22,31,45,11];
+
+function provjeri(x){
+  let pogodci = 0;
+  for(i=0;i<x.length;i++){
+    if(kombinacija.includes(x[i]))
+      ++pogodci;
+  }
+  return pogodci;
+}
+
+var igraci = [
+  {
+    ime: "Gojko",
+    prezime: "Sisa",
+    komb : [7,8,55,6,99,3],
+    kolikoPogodaka : function(){
+      console.log("Ima pogodaka: "+provjeri(this.komb));
+    }
+
+  },
+    {
+    ime: "Mujo",
+    prezime: "Mujic",
+    komb : [7,8,22,6,99,45],
+    kolikoPogodaka : function(){
+      console.log("Ima pogodaka: "+provjeri(this.komb));
+    }
+  }
+];
+
+igraci[0].kolikoPogodaka();
+igraci[1].kolikoPogodaka();
